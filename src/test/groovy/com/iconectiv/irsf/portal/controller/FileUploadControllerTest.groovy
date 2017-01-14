037d3a2a0471f8ae2f981b9ad691d5a32e2c8bb4
Eric Chang <echang@iconectiv.com>
2017-01-13 21:22:52 -0500

    update test case

--- src/test/groovy/com/iconectiv/irsf/portal/controller/FileUploadControllerTest.groovy
+++ src/test/groovy/com/iconectiv/irsf/portal/controller/FileUploadControllerTest.groovy
@@ -1,29 +1,21 @@
 package com.iconectiv.irsf.portal.controller
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpSession
 import org.springframework.mock.web.MockMultipartFile
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations=["classpath:spring-cfg.xml", "classpath:spring-jpa.xml"])
@@ -50,9 +42,12 @@ class FileUploadControllerTest extends GroovyTestCase {
 		MockMultipartFile thirdFile = new MockMultipartFile("file", "blacklist03.txt", "text/plain", "phone=7321010003".getBytes());
 		
 		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-		mockMvc.perform(MockMvcRequestBuilders.fileUpload("/uploadBlackList")
+		def action =  mockMvc.perform(MockMvcRequestBuilders.fileUpload("/uploadBlackList")
 						.file(firstFile).file(secondFile).file(thirdFile)
 						.param("customer", "cust03"));
+		def result = action.andReturn().getResponse().getContentAsString();
+		log.info(result)
+		assert result.indexOf("success") > 0
 		sleep(20*1000) 
 	}
 }
--- src/test/groovy/com/iconectiv/irsf/portal/controller/ListServiceControllerTest.groovy
+++ src/test/groovy/com/iconectiv/irsf/portal/controller/ListServiceControllerTest.groovy
@@ -1,27 +1,21 @@
 package com.iconectiv.irsf.portal.controller
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpSession
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;;;
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations=["classpath:spring-cfg.xml", "classpath:spring-jpa.xml"])
