11b9d001525ad621c248eb99d8a5f281c2dcc6d3
Eric Chang <echang@iconectiv.com>
2017-03-10 09:57:17 -0500

    update gitignore

--- src/main/java/com/iconectiv/irsf/portal/config/CustomerContextHolder.java
+++ src/main/java/com/iconectiv/irsf/portal/config/CustomerContextHolder.java
@@ -1,21 +1,1 @@
-package com.iconectiv.irsf.portal.config;
-
-public class CustomerContextHolder {
-	private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
-	
-	CustomerContextHolder() {
-		//default constructor
-	}
-	
-	public static void setCustomer(String customer) {
-		contextHolder.set(customer);
-	}
-	
-	public static String getCustomer() {
-		return contextHolder.get();
-	}
-	
-	public static void cleanCustomer() {
-		contextHolder.remove();
-	}
-}
+package com.iconectiv.irsf.portal.config;public class CustomerContextHolder {	private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();		CustomerContextHolder() {		//default constructor	}		public static void setCustomer(String customer) {		contextHolder.set(customer);	}		public static String getCustomer() {		return contextHolder.get();	}		public static void cleanCustomer() {		contextHolder.remove();	}}
