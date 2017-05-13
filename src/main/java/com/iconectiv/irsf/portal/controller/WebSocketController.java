ed4b58e0711d82035a41935fce1f7ef85ccad718
Eric Chang <echang@iconectiv.com>
2017-05-13 15:30:07 -0400

    test websocket

--- src/main/java/com/iconectiv/irsf/portal/controller/WebSocketController.java
+++ src/main/java/com/iconectiv/irsf/portal/controller/WebSocketController.java
@@ -1,29 +1,36 @@
 package com.iconectiv.irsf.portal.controller;
 
-import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import com.iconectiv.irsf.portal.core.EventTypeDefinition;
 import com.iconectiv.irsf.portal.model.common.EventNotification;
-import com.iconectiv.irsf.portal.service.EventNotificationService;
+import com.iconectiv.irsf.portal.repositories.common.EventNotificationRepository;
+import com.iconectiv.irsf.util.JsonHelper;
 
 @Controller
 public class WebSocketController {
 	private static Logger log = LoggerFactory.getLogger(WebSocketController.class);
 
-	@Autowired
-	EventNotificationService eventService;
 	
-    @SendTo("/topic/event")
-    public List<EventNotification> sendEvent() throws Exception {
-    	List<EventNotification> events = eventService.getEvents();
-    	
-        Thread.sleep(1000); // simulated delay
-        return events;
+	@Autowired
+	EventNotificationRepository eventRepo;
+	
+	@MessageMapping("/dataSetUpdate")
+    @SendTo("/topic/dataSetUpdateEvent")
+    public EventNotification sendDataSetUpdateEvent(SimpMessageHeaderAccessor headerAccessor) throws Exception {
+		Map<String, Object> attrs = headerAccessor.getSessionAttributes();
+		if (log.isDebugEnabled()) log.debug("receving web socket request: " + JsonHelper.toPrettyJson(attrs));
+		EventNotification event = eventRepo.findTop1ByEventTypeOrderByCreateTimestampDesc(EventTypeDefinition.MobileIdUpdate.value());
+
+        return event;
     }
 }
