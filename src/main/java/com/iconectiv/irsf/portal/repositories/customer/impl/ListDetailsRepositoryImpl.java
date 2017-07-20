8eb26fdb05de2da56e08d59afeb0ddb696494313
Eric Chang <echang@iconectiv.com>
2017-07-20 17:08:28 -0400

    change order

--- src/main/java/com/iconectiv/irsf/portal/repositories/customer/impl/ListDetailsRepositoryImpl.java
+++ src/main/java/com/iconectiv/irsf/portal/repositories/customer/impl/ListDetailsRepositoryImpl.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -33,7 +32,6 @@ public class ListDetailsRepositoryImpl implements ListDetailsRepositoryCustomer 
 	private int batchSize;
 	 
 	@Override
-	@Transactional
 	public void batchUpdate(Collection<ListDetails> entities) {
 		final List<ListDetails> savedEntities = new ArrayList<>(entities.size());
 		EntityManager entityManager = customerEntityManagerFactory.createEntityManager();
