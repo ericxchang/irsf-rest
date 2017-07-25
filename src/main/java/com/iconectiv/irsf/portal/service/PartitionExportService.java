d5426a97f2ce1b5ddead321f1dddffad59596721
Eric Chang <echang@iconectiv.com>
2017-07-25 14:16:39 -0400

    change to sync

--- src/main/java/com/iconectiv/irsf/portal/service/PartitionExportService.java
+++ src/main/java/com/iconectiv/irsf/portal/service/PartitionExportService.java
@@ -5,14 +5,12 @@ import com.iconectiv.irsf.portal.exception.AppException;
 import com.iconectiv.irsf.portal.model.common.CustomerDefinition;
 import com.iconectiv.irsf.portal.model.common.UserDefinition;
 import com.iconectiv.irsf.portal.model.customer.PartitionExportHistory;
-import org.springframework.scheduling.annotation.Async;
 
 /**
  * Created by echang on 5/28/2017.
  */
 public interface PartitionExportService {
 
-    @Async
     void resendPartition(UserDefinition loginUser, Integer exportPartitionId);
 
     void sendExportFile2EI(UserDefinition loginUser, PartitionExportHistory partHist, String url);
