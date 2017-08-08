ab2a411b25cb6afb148a3bb354ca528ce2e9f00f
whsiung <whsiung@iconectiv.com>
2017-08-08 09:42:34 -0400

    add \r to the error message

--- src/main/java/com/iconectiv/irsf/portal/core/MessageDefinition.java
+++ src/main/java/com/iconectiv/irsf/portal/core/MessageDefinition.java
@@ -21,7 +21,7 @@ public class MessageDefinition {
 	public static final String Delete_ListDetails_Success = "Successfully deleted list records";
 	public static java.lang.String Generating_Partition_Dataset_Success = "System is generating partition data set, it will take a moment";
     public static java.lang.String Exporting_Partition_Dataset_Success = "System is exporting partition data set";
-	public static final String ListSizeOverLimitError = "This $1 has reached the maximum of 100,000 rows.\nNo additional destination numbers can be added.";
+	public static final String ListSizeOverLimitError = "This $1 has reached the maximum of 100,000 rows.\r\nNo additional destination numbers can be added.";
 	public static final String Remove_Rule =  "Successfully removed rule from partition";
 
 	public static final String DELETE_PARTITION = "Successfully delete all rules from partition";
