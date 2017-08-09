fff3d7b63fe5e6325630c713f41bb51f321d46e3
Eric Chang <echang@iconectiv.com>
2017-08-09 14:42:09 -0400

    synchronize request

--- src/main/java/com/iconectiv/irsf/portal/service/impl/ListServiceImpl.java
+++ src/main/java/com/iconectiv/irsf/portal/service/impl/ListServiceImpl.java
@@ -26,7 +26,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -88,7 +87,7 @@ public class ListServiceImpl implements ListService {
         }
     }
 
-    @Async
+    //@Async
     @Override
     public void processListUploadRequest(UserDefinition user, ListDefinition listDef, ListUploadRequest uploadRequest, boolean isInitialLoading, String errorMessage) {
         try {
