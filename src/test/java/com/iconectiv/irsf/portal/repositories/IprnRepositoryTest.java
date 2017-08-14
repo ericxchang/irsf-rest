048916396cfb8a6996cfa92ebf330b5d0708f212
Eric Chang <echang@iconectiv.com>
2017-08-14 11:53:27 -0400

    update format

--- src/test/java/com/iconectiv/irsf/portal/repositories/IprnRepositoryTest.java
+++ src/test/java/com/iconectiv/irsf/portal/repositories/IprnRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.iconectiv.irsf.portal.repositories;
 
+import com.iconectiv.irsf.portal.model.common.Premium;
+import com.iconectiv.irsf.portal.repositories.common.PremiumRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -29,12 +31,21 @@ public class IprnRepositoryTest {
 	
 	@Autowired
 	IprnRepository repository;
+	@Autowired
+	PremiumRepository preRepo;
 	
 	@Test
 	public void testQueryIprn() throws Exception {
 		PageRequest page = new PageRequest(3, 5);
 		Page<Iprn> result = repository.findAll(page);
 		log.info(JsonHelper.toJson(result));
+	}
+
+	@Test
+	public void testQueryPremium() throws Exception {
+		PageRequest page = new PageRequest(3, 5);
+		Page<Premium> result = preRepo.findAll(page);
+		log.info(JsonHelper.toPrettyJson(result));
 	}
 
 }
